6d7ba4fb54a66686e643900554f63514e92c4d24
thakurutkarsh22 <thakurutkarsh22@gmail.com>
2019-02-15 17:07:49 +0530

    Editing Auth Controller using Form-body

--- WebAPIforCRUD/Controllers/AuthsController.cs
+++ WebAPIforCRUD/Controllers/AuthsController.cs
@@ -7,6 +7,7 @@ using System.Net.Http;
 using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
 using System.Web.Http;
 using System.Web.Http.Results;
 using System.Web.Mvc;
@@ -19,25 +20,29 @@ namespace WebAPIforCRUD.Controllers
     public class AuthsController : ApiController
     {
         [System.Web.Http.HttpPost]
-        public async Task<IActionResult> TokenAsync()
+        public async Task<HttpResponseMessage> TokenAsync()
         {
-
+            
             var header = Request.Headers.Authorization;
-            if (header.ToString().StartsWith("Basic"))
+            if (header.ToString().StartsWith("Inherit"))
             {
-                var credValue = header.ToString().Substring("Basic ".Length).Trim();
+
+            /*    var credValue = header.ToString().Substring("Basic ".Length).Trim();
                 var usernameAndPassenc = Encoding.UTF8.GetString(Convert.FromBase64String(credValue)); //admin:pass
                 var usernameAndPass = usernameAndPassenc.Split(':');
                 //check in DB username and pass exist
+                */
+                var name = HttpContext.Current.Request.Form["Login"];
+                var pass = HttpContext.Current.Request.Form["Password"]; 
+
+
+
 
                 AuthRepository ap = new AuthRepository();
 
-
-
-
-                if ((await ap.GetUserAsync(usernameAndPass[0], usernameAndPass[1])) != null)
+                if ((await ap.GetUserAsync(name, pass)) != null)
                 {
-                    var claimsdata = new[] { new Claim(ClaimTypes.Name, usernameAndPass[0]) };
+                    var claimsdata = new[] { new Claim(ClaimTypes.Name, name) };
                     var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("ahbasshfbsahjfbshajbfhjasbfashjbfsajhfvashjfashfbsahfbsahfksdjf"));
                     var signInCred = new SigningCredentials(key, SecurityAlgorithms.HmacSha256Signature);
                     var token = new JwtSecurityToken(
@@ -57,11 +62,11 @@ namespace WebAPIforCRUD.Controllers
                     jsres.description = "Token is Succesfully Generated";
                     jsres.status = "Success";
                     jsres.token = tokenString + "";
-                    
+
+                    return Request.CreateResponse(HttpStatusCode.OK , jsres);
 
 
-
-                    return new Microsoft.AspNetCore.Mvc.JsonResult(jsres);
+                  //  return new Microsoft.AspNetCore.Mvc.JsonResult(jsres);
                 }
                 else
                 {
@@ -69,11 +74,12 @@ namespace WebAPIforCRUD.Controllers
                     jsres.description = "Username or password is incorrect";
                     jsres.status = "Failed";
 
-                    return new Microsoft.AspNetCore.Mvc.JsonResult(jsres);
+                    //return new Microsoft.AspNetCore.Mvc.JsonResult(jsres);
+                    return Request.CreateResponse(HttpStatusCode.OK, jsres);
                 }
             }
 
-            return null;
+            return Request.CreateResponse(HttpStatusCode.ExpectationFailed,"This is garbage");
         }
 
     }
