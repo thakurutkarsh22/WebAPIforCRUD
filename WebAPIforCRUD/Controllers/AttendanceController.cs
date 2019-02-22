c22448ec306ad597f443553118a7e550491c30b6
thakurutkarsh22 <thakurutkarsh22@gmail.com>
2019-02-23 04:40:49 +0530

    attendance controller update

--- WebAPIforCRUD/Controllers/AttendanceController.cs
+++ WebAPIforCRUD/Controllers/AttendanceController.cs
@@ -14,7 +14,7 @@ using MongoDB.Bson;
 
 namespace WebAPIforCRUD.Controllers
 {
-    
+    [Authorize]
     public class AttendanceController : ApiController
     {
 
@@ -45,21 +45,23 @@ namespace WebAPIforCRUD.Controllers
         {
             // get all student accrding to class 
             var sfilter = Builders<studentViewModel>.Filter.Eq("Class_id", Class_id);
-            IList<studentViewModel> stulist = studentCollection.Find(sfilter).ToList();
+            List<studentViewModel> stulist = studentCollection.Find(sfilter).ToList();
 
 
             var Filter = Builders<attendanceViewModel>.Filter.Eq("class_id", Class_id);
-            IList<attendanceViewModel> list = attendanceCollection.Find(Filter).ToList();
-            
-            IList<attendanceViewModel1> flist = new List<attendanceViewModel1>();
+            List<attendanceViewModel> list = attendanceCollection.Find(Filter).ToList();
+            List<attendanceViewModel> attenList = attandanceList(list , date);
+            List<attendanceViewModel1> a = mainFunction(stulist, attenList); 
 
-            foreach(var student in stulist)
+   //         IList<attendanceViewModel1> flist = new List<attendanceViewModel1>();
+
+        /*    foreach(var student in stulist)
             {
                 Boolean flag = false;
                 foreach (var attendance in list)
                 {
                     string tempDate = Convert.ToString(attendance.dateOfAttendance.AddDays(1)).Split(' ')[0];
-                    if (student._id.ToString().Equals(attendance.studend_id) && string.Compare(date, tempDate) == 0) // student._id.ToString().Equals(attendance.studend_id) && 
+                    if (student._id.ToString().Equals(attendance.studend_id)) //  && string.Compare(date, tempDate) == 0   student._id.ToString().Equals(attendance.studend_id) && 
                     {
                         flag = true;
                         break;                                                    
@@ -93,7 +95,7 @@ namespace WebAPIforCRUD.Controllers
                     //notAtten._id = ss._id;
                     flist.Add(notAtten);
                 }
-            }
+            }*/
         /*    attendanceViewModel a1 = null;
             foreach(var ss in list)
             {
@@ -115,8 +117,79 @@ namespace WebAPIforCRUD.Controllers
                 }
             }*/
 
-            return Ok(flist); 
+            return Ok(a); 
         }
+
+        private List<attendanceViewModel> attandanceList(IList<attendanceViewModel> list ,string date )
+        {
+            List<attendanceViewModel> tempList = new List<attendanceViewModel>();
+
+
+            foreach (var ss in list)
+            {
+                string datefrommodel = Convert.ToString(ss.dateOfAttendance).Split(' ')[0];
+                if (datefrommodel.CompareTo(date) == 0)
+                {
+                    tempList.Add(ss);
+                }
+                else
+                {
+                    continue;
+                }
+            }
+
+
+            return tempList;
+        }
+
+        private List<attendanceViewModel1> mainFunction(List<studentViewModel> stulist,IList<attendanceViewModel> attenList)
+        {
+            List<attendanceViewModel1> flist = new List<attendanceViewModel1>();
+
+            if (attenList.Count == 0)
+            {
+                flist = convertToJsonforattenLsitzero(stulist);
+            }else
+            {
+                foreach(var ss in stulist)
+                {
+                    int count = 0;
+                    foreach(var aa in attenList)
+                    {
+                        if (ss._id.ToString().CompareTo(aa.studend_id) == 0)
+                        {
+                            count++;
+                            flist.Add(makeobject( ss._id.ToString(),ss.stuName,aa.AttendanceMark,ss.Class_id));
+                        }
+                    }
+                    if (count == 0)
+                    {
+                        flist.Add(makeobject(ss._id.ToString(), ss.stuName ,"false",ss.Class_id));
+                    }
+                }
+            }
+            return flist;
+        }
+
+        private List<attendanceViewModel1> convertToJsonforattenLsitzero(List<studentViewModel> list)
+        {
+            List<attendanceViewModel1> flist = new List<attendanceViewModel1>();
+            attendanceViewModel1 a = null;
+            foreach(var ss in list)
+            {
+                a = new attendanceViewModel1(ss._id.ToString() , ss.stuName , "false",  ss.Class_id);
+                flist.Add(a);
+            }
+
+            return flist; 
+        }
+
+        private attendanceViewModel1 makeobject(string stu_id , string stu_name , string attendancemark , string class_id)
+        {
+            attendanceViewModel1 a = new attendanceViewModel1(stu_id,stu_name,attendancemark,class_id);
+            return a; 
+        }
+
 
         // POST api/<controller>
         public IHttpActionResult Post([FromBody]JArray value)
--- WebAPIforCRUD/Models/attendanceViewModel1.cs
+++ WebAPIforCRUD/Models/attendanceViewModel1.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,12 +9,21 @@ namespace WebAPIforCRUD.Models
 {
     public class attendanceViewModel1
     {
+       
         public string studend_id { get; set; }
+
+        public string stu_name { get; set; }
 
         public string AttendanceMark { get; set; }
 
-        public DateTime dateOfAttendance { get; set; }
-
         public string class_id { get; set; }
+
+        public attendanceViewModel1(string student_id1 , string stu_name1 , string attendanceMark, string class_id1)
+        {
+            this.studend_id = student_id1;
+            this.stu_name = stu_name1;
+            this.AttendanceMark = attendanceMark;
+            this.class_id = class_id1; 
+        }
     }
 }
